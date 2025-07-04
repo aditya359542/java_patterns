c086b2ce0d92fef20783d999a2ed1e8697174706
aditiya359542 <adityaseth26052002@gmail.com>
2025-07-05 04:27:45 +0530

    Question 7

--- patterns.java
+++ patterns.java
@@ -149,6 +149,35 @@ public class patterns {
 //        }
 
 
+//        Qustion 7
+
+
+//        1 2 3 4 5
+//        1 2 3 4
+//        1 2 3
+//        1 2
+//        1
+
+
+//        System.out.println("lenght ??");
+//        int length = sc.nextInt();
+//
+//        for (int i = length ; i>= 1 ; i--){
+//            for (int j = 1 ; j <= i ; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+
+
+
+
+
 
     }
 
