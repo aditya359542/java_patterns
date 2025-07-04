9381d625e70e8070c559488f8d5ee14d8e76522a
aditiya359542 <adityaseth26052002@gmail.com>
2025-07-05 02:54:26 +0530

    star patterns

--- patterns.java
+++ patterns.java
@@ -1,19 +1,49 @@
+import java.util.Scanner;
+
 public class patterns {
 
     public static void main(String[] args) {
-        int n = 5; // Number of rows
-        for (int i = 1; i <= n; i++) {
-            // Print leading spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // Print stars
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
-            // Move to the next line
-            System.out.println();
-        }
+       
+        Scanner sc = new Scanner(System.in);
+
+
+// Question1
+
+//* * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+
+//         int number = sc.nextInt();
+//         for (int i = 1 ; i <= number; i++){
+// System.out.println("* * * * *");
+//         }
+
+
+
+// Question2
+
+
+int number = sc.nextLine();
+
+for(int i = 1 ; i<= number; i++)
+
+
+
+
+
+
+// jj
+
+
+
+
+
+
+
+
     }
     
 }
