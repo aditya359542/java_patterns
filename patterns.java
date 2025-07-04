ac8365a8dc2468ac687805d477af052af6e3f200
aditiya359542 <adityaseth26052002@gmail.com>
2025-07-05 04:16:37 +0530

    Question 6

--- patterns.java
+++ patterns.java
@@ -127,11 +127,30 @@ public class patterns {
 //}
 
 
+//        Question 6
 
 
 
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
 
-        }
+
+//        System.out.println("lenght ??");
+//        int length = sc.nextInt();
+//
+//        for (int i = 1 ; i <=length ; i++){
+//            for (int j = 1 ; j <= i ; j++){
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+    }
 
 
 
