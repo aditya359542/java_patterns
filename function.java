c11b2b6348ce64660bb026ec27551c81bdf27ec2
aditiya359542 <adityaseth26052002@gmail.com>
2025-07-10 00:51:44 +0530

    Function

--- function.java
+++ function.java
@@ -3,21 +3,35 @@ import java.util.Scanner;
 public class function {
 
 
-    static int sum(int a, int b){
-      return a*b;
+    // static int sum(int a, int b){
+    //   return a*b;
+    // }
+
+    static int fact(int a){
+
+        int num = 1;
+        for (int i = num ; i >= 1; i--){
+            num = num * i;
+            System.out.println(num);
+        }
+
+
+
+        
+        return num;
     }
+
 
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("provide two numbers");
+        System.out.println("provide numbers");
         int a = sc.nextInt();
-        int b = sc.nextInt();
 
 
-        System.out.println(sum(a,b));
+        System.out.println(fact(a));
 
 
 
