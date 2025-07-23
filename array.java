1ac0366a13bf4086a2b0328a76aa5a27feb52619
aditiya359542 <adityaseth26052002@gmail.com>
2025-07-24 00:44:34 +0530

    array question linear search

--- array.java
+++ array.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 public class array {
 
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Array size");
+
+
+
+        int[] arr = new int[sc.nextInt()];
+
+        for (int i= 0 ; i < arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("value you find");
+
+        int find = sc.nextInt();
+
+        for (int i= 0 ; i <  arr.length ; i++){
+
+        if (arr[i] == find){
+            System.out.println(i);
+        }
+
+        }
+
+
     }
 }
